20ece1562d36b41e36384b9483f230c5127f506a
sungin <sijm@naver.com>
2025-02-28 19:02:04 +0900

    geistSans 삭제

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,15 +5,6 @@ import "./globals.css";
 import Head from 'next/head';
 import { ReactNode } from 'react';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 // export const metadata: Metadata = {
 //   title: "자이 에이스 부동산",
