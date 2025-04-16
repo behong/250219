59748085c92e7abdfbe234c24d083dafa949a788
hongsungin <nayha77@gmail.com>
2025-04-16 10:41:11 +0900

    sitemap 수정

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://your-domain.com',
+  siteUrl: 'https://dcace.vercel.app',
   generateRobotsTxt: true,
   sitemapSize: 7000,
 } 
